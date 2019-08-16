1787c4402104fef91481ce54ba6711d0b56e15e4
Carlos Martinez <martinezcj2@gmail.com>
2019-08-16 00:30:11 -0400

    Create Goal list with scroll capabilities

--- App.js
+++ App.js
@@ -1,23 +1,60 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('yerrr!');
-  console.log(outputText);
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState(['Get a fulfilling job']);
+
+  changeTextHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  submitGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal])
+    setEnteredGoal('');
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>{ outputText }</Text>
-      <Button title="lit button" onPress={() => setOutputText('even litter boiii') } />
+    <View style={styles.container} >
+      <View style={styles.inputGroup} >
+        <TextInput
+          placeholder='Enter course goal'
+          style={styles.textInput}
+          value={enteredGoal}
+          onChangeText={changeTextHandler}
+        />
+        <Button title='ADD' onPress={submitGoalHandler} />
+      </View>
+      <ScrollView>
+        {
+          courseGoals.map((goal, i) => (
+            <View style={styles.listItem} key={i}><Text>{goal}</Text></View>
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50
   },
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textInput: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: '80%'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#efefef',
+    borderColor: '#ccc',
+    borderWidth: 1
+  }
 });
